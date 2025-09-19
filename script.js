90373c91275226a4fca5ad3767bd77f6106b6689
Zack Jorquera <jorquerazack@gmail.com>
2025-09-19 13:58:20 -0700

    Updated index

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function changeHeadshotImg1(){
-    document.getElementById("headshotimg").src = "images/kayak_bailey.jpg";
-    document.getElementById("aboutTextHeadshot").textContent = "The Supermax Rapid in Bailey Canyon, CO (Class V)";
+    document.getElementById("headshotimg").src = "images/kayak_49b.jpg";
+    document.getElementById("aboutTextHeadshot").textContent = 'The "Not Tiger Tail" Rapid on 49 to Bridgeport (Class V)';
 };
 function changeHeadshotImg2(){
     document.getElementById("headshotimg").src = "images/headshot.jpg";
