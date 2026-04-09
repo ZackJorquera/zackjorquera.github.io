8cdf734e89e2442bdc40492a4add22f1e61d0d2c
Zack Jorquera <jorquerazack@gmail.com>
2026-04-09 14:01:42 -0700

    Minor changes. Added images.

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+const randomImgChoice = (() => {
+  let value;
+  return () => value ??= Math.floor(Math.random() * 3) + 1;
+})();
 function changeHeadshotImg1(){
-    document.getElementById("headshotimg").src = "images/kayak_49b.jpg";
-    document.getElementById("aboutTextHeadshot").textContent = 'The "Not Tiger Tail" Rapid on 49 to Bridgeport (Class V)';
+    switch (randomImgChoice()) {
+        case 1:
+            document.getElementById("headshotimg").src = "images/kayak_49b.jpg";
+            document.getElementById("aboutTextHeadshot").textContent = 'The Meltdown rapid on the 49 to Bridgeport section of the South Yuba.';
+            break;
+        case 2:
+            document.getElementById("headshotimg").src = "images/kayak_bailey.jpg";
+            document.getElementById("aboutTextHeadshot").textContent = 'The Supermax rapid on Bailey Canyon in Colorado.';
+            break;
+        case 3:
+            document.getElementById("headshotimg").src = "images/kayak_merced.jpg";
+            document.getElementById("aboutTextHeadshot").textContent = 'The Sasquatch rapid on the Park Boundry section of the Merced River.';
+            break;
+        default:
+            break;
+    }
 };
 function changeHeadshotImg2(){
     document.getElementById("headshotimg").src = "images/headshot.jpg";
